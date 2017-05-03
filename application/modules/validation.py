7a13f376f382756111fed6db62e4b06442a74765
Yugo Kato <yugokato.o@gmail.com>
2017-05-03 10:11:41 -0700

    Changed the mechanism to check AWS ip

--- application/modules/validation.py
+++ application/modules/validation.py
@@ -2,9 +2,10 @@
 import os
 import re
 import socket
-
+from application import app
 
 class Validation(object):
+    __is_connected = None
     __aws_cache = []    # cache for AWS IP Address
     __no_aws_cache = [] # cache for non-AWS IP Address
 
@@ -31,14 +32,33 @@ class Validation(object):
     # check if the IP Address is from AWS cloud
     @staticmethod
     def is_aws(ipaddr):
-        if ipaddr in Validation.__aws_cache:
-            return True
-        elif ipaddr in Validation.__no_aws_cache:
-            return False
-        else:   # if ipaddr not in chache
-            for line in os.popen("nslookup %s " % ipaddr):
-                if 'compute.amazonaws.com' in line:
-                    Validation.__aws_cache.append(ipaddr)
-                    return True
-            Validation.__no_aws_cache.append(ipaddr)
-            return False
+        if Validation.check_internet_connection():
+            if ipaddr in Validation.__aws_cache:
+                return True
+            elif ipaddr in Validation.__no_aws_cache:
+                return False
+            else:   # if ipaddr not in chache
+                try:
+                    result = socket.gethostbyaddr(ipaddr)
+                    for line in result:
+                        if 'compute.amazonaws.com' in line:
+                            Validation.__aws_cache.append(ipaddr)
+                            return True
+                except socket.herror:
+                    Validation.__no_aws_cache.append(ipaddr)
+        return False
+
+
+    @staticmethod
+    def check_internet_connection():
+        if Validation.__is_connected == None:
+            try:
+                socket.setdefaulttimeout(3)
+                socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+                Validation.__is_connected = True
+                return True
+
+            except:
+                app.logger.warning("Internet connection is not available")
+                Validation.__is_connected = False
+        return Validation.__is_connected
